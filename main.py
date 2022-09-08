118c9f25e7f9656a80a3f13be38c19a1c5949316
magique <kadirbey832832@gmail.com>
2022-09-08 21:10:35 +0300

    Updated for more neatness and readability.

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ list1 = [[1,'a',['cat'],2],[[[3]],'dog'],4,5]
 list2 = [[1, 2], [3, 4], [[5, [6, 10, 16]], 7]]
 
 print("Original list1 : ", list1)
-print("Flatten version of list1 : ", list(flattenFunction.flatten(list1)))
+print("Flatten version of list1 : ", list(FlattenFunction.flatten(list1)))
 list(ListReverserFunction.listReverserFunc(list1))
 print("Reversed version of list1 : ", list1)
 
 print("----------------------------------------------")
 print("Original list2 : ", list2)
-print("Flatten version of list2 : ", list(flattenFunction.flatten(list2)))
+print("Flatten version of list2 : ", list(FlattenFunction.flatten(list2)))
 list(ListReverserFunction.listReverserFunc(list2))
 print("Reversed version of list2 : ", list2)
